6635529386df302e2b071fee02b64969055cc9b4
Tim Wheeler <tim@timwheeler.com>
2019-06-26 21:40:14 -0400

    updates
    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase')
 const config = {
-	apiKey: "AIzaSyAcIFJAkCSk_RhODZVlmpga_msPQ7AnWQA",
+	apiKey: <API_KEY>,
 	authDomain: "http://localhost:3000",
 	databaseURL: "https://ouradventures-1542167907279.firebaseio.com",
 	projectId: "ouradventures-1542167907279",
